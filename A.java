a1ca1e36e5bc89017128d8eee32f883e3618b616
znchn <zwt15251899489@163.com>
2022-03-10 00:28:28 +0800

    add A.java

---
+++ A.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class A{
+    public static void main(String[] args) {
+        int[] arr = new int[]{5, 4, 3, 2, 1};
+        for (int i = 0; i < arr.length; i++){
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[i] > arr[j]){
+                    int k = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = k;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
